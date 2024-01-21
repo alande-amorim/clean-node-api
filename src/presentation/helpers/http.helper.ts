089f26641840686616ff9650b035ad2cca25b0a5
Alande Amorim <alande.amorim@gmail.com>
2024-01-21 08:48:02 -0300

    refactor: create serverError and success responses factories

--- src/presentation/helpers/http.helper.ts
+++ src/presentation/helpers/http.helper.ts
@@ -1,5 +1,16 @@
+import { ServerError } from '../errors/server.error'
 import { type HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400, body: error
+})
+
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
+})
+
+export const success = (body?: any): HttpResponse => ({
+  statusCode: 200,
+  body
 })
